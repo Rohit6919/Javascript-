a2b7b5cf5467997e58a6a4092f64ccaf39487652
Rohit6919 <patelrohit6919@gmail.com>
2025-02-03 09:43:32 +0000

    commit changes

--- .devcontainer/01_basics/03_ConversionOperator.js
+++ .devcontainer/01_basics/03_ConversionOperator.js
@@ -1,12 +1,12 @@
-let score = "33"
-let valuechange= Number(score)
-console.log(typeof valuechange);
-// ouput is number
+// let score = "33"
+// let valuechange= Number(score)
+// console.log(typeof valuechange);
+// // ouput is number
 
-let score1 = "33abc"
-let valuechange1= Number(score1)
-console.log(typeof valuechange1);
-console.log(valuechange1);
+// let score1 = "33abc"
+// let valuechange1= Number(score1)
+// console.log(typeof valuechange1);
+// console.log(valuechange1);
 
 // if we dont give proper number this it is give as 
 // NaN not  a number output ,  if we print it will
@@ -16,4 +16,18 @@ console.log(valuechange1);
 // "33abc"=> nan
 // true =>1; false =>0
 
+
+
+//  ************************* Operations ***********************
+
+// let str1 = "hello"
+// let str2 = "tejas"
+// let str3 = str1 + str2;
+// console.log(str3);
+ 
+// console.log("1" + 1);  -->11 concatinate
+// console.log(1 + "2");  -->12
+// console.log("1" + 2 + 2); -->122
+
+// console.log(+true); --> 1 output 
  
