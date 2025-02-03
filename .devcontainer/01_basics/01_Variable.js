a3862f396cbb40ed0ff5ef4d7cf70588834e1307
Rohit6919 <patelrohit6919@gmail.com>
2025-02-03 07:15:34 +0000

    commit changes

---
+++ .devcontainer/01_basics/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId=12344
+let accountEmail="tejas@gmail.com"
+var accounpass="123" 
+acountcity="Surat"
+
+console.table([accountId,accountEmail,accounpass,acountcity])
+
+/*
+prefer not to use var
+bacause of issue in block scope and function scope
+*/
---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert (3 + 3) we are using node js , not browser so this will not run
+
+// number
+// boolean
+// string
+// bigint
+// undefined 
+// null
+// symbol 
+
+console.log(typeof null); //ouput ->Object 
+console.log(typeof undefined); //output-> undefined
