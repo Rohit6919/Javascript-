88ea9bca11982c168b7da0d00a2dda3801969d8a
Rohit6919 <patelrohit6919@gmail.com>
2025-02-03 07:41:45 +0000

    commit changes

---
+++ .devcontainer/01_basics/03_ConversionOperator.js
@@ -0,0 +1,19 @@
+let score = "33"
+let valuechange= Number(score)
+console.log(typeof valuechange);
+// ouput is number
+
+let score1 = "33abc"
+let valuechange1= Number(score1)
+console.log(typeof valuechange1);
+console.log(valuechange1);
+
+// if we dont give proper number this it is give as 
+// NaN not  a number output ,  if we print it will
+// give NaN this output  
+
+// "33"=> 33
+// "33abc"=> nan
+// true =>1; false =>0
+
+ 
