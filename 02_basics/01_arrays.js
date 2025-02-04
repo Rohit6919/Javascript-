d14f5ccc4aa1d11ed3394d9dec08245b655f1f82
Rohit6919 <patelrohit6919@gmail.com>
2025-02-04 08:58:24 +0000

    new changes

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,32 @@
 //array
 
 const myArr = [0,1,2,3,4,5]
-console.log(myArr[0]);
+// console.log(myArr[0]);
+
+//array methods
+// myArr.push(6);
+// myArr.push(7);--> addd at the last
+// myArr.pop()  --> remove from the last
+// console.log(myArr);
+
+// myArr.unshift(9); --> Ad at the first elementof the array
+// myArr.shift(); 
+console.log(myArr);
+
+// const newArr = myArr.join()
+// console.log(typeof newArr); ---convert in String
+
+// slice and splice
+
+console.log("A" , myArr); 
+
+// const my2 = myArr.splice(1,3)
+// console.log("b",my2);
+// console.log(myArr);
+// remove the item from the array
+
+
+// const my2 = myArr.slice(1,3)
+// console.log("b",my2);
+// console.log(myArr);
+// dont not remove anything from the array
