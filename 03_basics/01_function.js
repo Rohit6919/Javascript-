7ef5676a278cc4ed33b1087e8c5b1feed2782a44
Rohit6919 <patelrohit6919@gmail.com>
2025-02-04 19:17:28 +0000

    commit changes

---
+++ 03_basics/01_function.js
@@ -0,0 +1,15 @@
+
+function myName() {
+
+console.log("T");
+console.log("e");
+console.log("j");
+console.log("a");
+console.log("s");
+}
+myName();
+
+function addTwoNumber(number1,number2){
+   console.log( number1 + number2);
+}
+addTwoNumber(2, null)
