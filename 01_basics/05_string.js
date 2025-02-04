17d4018dc55430ce69045d3545f0a6eb752c2eaf
Rohit6919 <patelrohit6919@gmail.com>
2025-02-04 07:35:06 +0000

    new update

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const name="tejas"
+const repoCount = 50
+
+// console.log(name + repoCount + "value")
+
+// ( ``)-> backtick is called and used for interpolation you can do many things onthe go
+
+const gameName = new String("tejas")
+console.log(gameName[0]); 
+console.log(gameName.charAt(2))
+
+const newString= gameName.substring(0, 4)
+console.log(newString);
